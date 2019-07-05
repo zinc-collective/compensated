748e275375b5445c55bde8bbbdfec252d6d51e34
Zee <zee@zincma.de>
2019-07-05 16:40:27 -0700

    Demonstrate handing exceptions from within compensated
    Ultimately the decision is up to the consumer of the compensated gem to
    figure out what they should do in the event that compensated raises an
    error.

--- examples/compensated-rails-5/app/controllers/payment_processor_events_controller.rb
+++ examples/compensated-rails-5/app/controllers/payment_processor_events_controller.rb
@@ -22,14 +22,28 @@ class PaymentProcessorEventsController < ApplicationController
       # We encourage you to return a 5XX status code to trigger
       # the retry behavior.
       head :internal_server_error
-    rescue Compensated::Error
-      # If you don't care about particular errors
-      # You can return `ok`.
-
-      # However, this does mean that you could "miss" events that you really
-      # wanted to capture, but didn't notice because they were discarded
-      # quietly.
-      head :ok
     end
+
+  # You will want to make a decision about what you want to do in the event
+  # that compensated throws an error. At the very least, you will want
+  # to send information to your exception tracker.
+  rescue Compensated::Error => _
+    # If you don't care about particular errors
+    # You can return a 200 success.
+    #
+    # head :ok
+    #
+    # However, this means you could "miss" events that you really
+    # wanted to capture, but didn't notice because they were discarded
+    # quietly.
+
+    # If you want to allow the payment processor's default failure handling
+    # behavior to execute, you will want to return a 500
+    #
+    # head :internal_server_error
+    #
+    # However, you will want to make sure you are rolling back any transactions
+    # and/or ensuring you didn't spawn any jobs that would cause a side effect;
+    # as the payment processor may retry sending the event.
   end
 end
