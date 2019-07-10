6affaf1911951fb6ed524f3cbe40894ed5cc7182
Zee <zee@zincma.de>
2019-07-10 14:18:50 -0700

    The name feels better than making it match stripes arbitrarily

--- compensated-ruby/lib/compensated/gumroad/csv_emitter.rb
+++ compensated-ruby/lib/compensated/gumroad/csv_emitter.rb
@@ -5,7 +5,7 @@ module Compensated
       def parse(io)
         CSV.new(io, headers: true).map do |row|
           {payment_processor: :gumroad,
-           raw_event_type: :"purchase.completed",
+           raw_event_type: :"csv.purchase_entry",
            raw_event_id: row["Purchase ID"],
            raw_body: row.to_s.strip,}
         end
--- compensated-ruby/spec/gumroad/csv_emitter_spec.rb
+++ compensated-ruby/spec/gumroad/csv_emitter_spec.rb
@@ -11,13 +11,13 @@ module Compensated
           events = emitter.parse(load_csv("example-export.csv"))
           expect(events[0]).to include({
             payment_processor: :gumroad,
-            raw_event_type: :"purchase.completed",
+            raw_event_type: :"csv.purchase_entry",
             raw_event_id: "pur_fake-1",
             raw_body: 'pur_fake-1,Support,,person@example.com,false,2017-08-02,2017-08-02 16:26:27 +0000,10.0,0.0,0.0,10.0,0.65,9.35,,"","",12345,CA,United States,https://www.google.com/,0,0,(Bicycle),"",false,false,huxLm,661264979,"",{},5.0,5.0,false,"","",1,monthly,"",0.0,false,2019-02-02,""',
           })
           expect(events[1]).to include({
             payment_processor: :gumroad,
-            raw_event_type: :"purchase.completed",
+            raw_event_type: :"csv.purchase_entry",
             raw_event_id: "pur_fake-2",
             raw_body: 'pur_fake-2,Support,,person@example.com,false,2017-09-02,2017-09-02 16:26:32 +0000,10.0,0.0,0.0,10.0,0.65,9.35,,"","",12345,CA,United States,https://www.google.com/,0,0,(Bicycle),"",true,false,huxLm,453382483,"",{},5.0,5.0,false,"","",1,monthly,"",0.0,false,2019-02-02,""',
           })
