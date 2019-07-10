df5d316c480c12860967de0f36cdf0302560a8b5
Zee <zee@zincma.de>
2019-07-10 16:53:18 -0700

    Support events that use form data

--- compensated-ruby/lib/compensated/payment_processor_event_request_handler.rb
+++ compensated-ruby/lib/compensated/payment_processor_event_request_handler.rb
@@ -1,3 +1,4 @@
+require_relative "payment_processor_event_request"
 module Compensated
   # Normalizes events by passing them to an EventParser
   # Which transforms them into a hash that can be
@@ -7,28 +8,19 @@ module Compensated
     # @param event_request [Request] A rack-compatible HTTP request triggered
     #                                by the payment processor
     def initialize(event_request)
-      self.event_request = event_request
+      self.event_request = PaymentProcessorEventRequest.new(event_request)
     end
 
     # @return Hash
     def normalized_event_data
-      event_parser.parse(event_request_body_data)
+      event_parser.parse(event_request)
     end
 
     protected def event_parser
       return @event_parser unless @event_parser.nil?
-      @event_parser = Compensated.event_parsers.find { |parser| parser.parses?(event_request_body_data) }
-      raise NoParserForEventError, event_request_body_data if @event_parser.nil?
+      @event_parser = Compensated.event_parsers.find { |parser| parser.parses?(event_request) }
+      raise NoParserForEventError, event_request.data if @event_parser.nil?
       @event_parser
-    end
-
-    protected def event_request_body_data
-      @event_request_body_data ||= Compensated.json_adapter.parse(event_request_body)
-    end
-
-    protected def event_request_body
-      return @event_request_body unless @event_request_body.nil?
-      @event_request_body = event_request.body
     end
   end
 end
