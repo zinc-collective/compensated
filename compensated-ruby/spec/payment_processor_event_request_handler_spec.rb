4f38e2fccc144f1195e96483414733f631f83e0e
Tom Lee <user512@users.noreply.github.com>
2020-04-16 07:33:02 -0700

    Deprecate PaymentProcessorEventRequestHandler#normalized_event_data

--- compensated-ruby/spec/payment_processor_event_request_handler_spec.rb
+++ compensated-ruby/spec/payment_processor_event_request_handler_spec.rb
@@ -1,19 +1,19 @@
 require "compensated"
 module Compensated
   RSpec.describe PaymentProcessorEventRequestHandler do
-    describe "#normalized_event_data" do
+    describe "#transformed_event_data" do
       let(:handler) { described_class.new(double(form_data?: true, params: {field: :value})) }
       context "when it can be parsed" do
         it "passes the request through to the parser" do
           allow(Compensated).to receive(:event_parsers).and_return([double(parses?: true, parse: :data!)])
-          expect(handler.normalized_event_data).to eql(:data!)
+          expect(handler.transformed_event_data).to eql(:data!)
         end
       end
 
       context "when the event cannot be parsed" do
         it "Raises an exception" do
           allow(Compensated).to receive(:event_parsers).and_return([double(parses?: false)])
-          expect { handler.normalized_event_data }.to raise_error(NoParserForEventError)
+          expect { handler.transformed_event_data }.to raise_error(NoParserForEventError)
         end
       end
     end
