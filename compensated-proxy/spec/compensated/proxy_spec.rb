8549f34a6c6d290249f76ce6bd06fafa1fb4f7de
Zee Spencer <50284+zspencer@users.noreply.github.com>
2020-05-17 10:52:56 -0700

    Don't use hardcoded strings because they don't play nice with timezones

--- compensated-proxy/spec/compensated/proxy_spec.rb
+++ compensated-proxy/spec/compensated/proxy_spec.rb
@@ -17,16 +17,18 @@ RSpec.describe Compensated::Proxy do
   let(:request) { fake_request(template_file_path, interpolate: interpolate) }
   let(:fixture) { "customer.subscription.updated.api-v2019-12-03.json" }
 
-  it "forwards the standardized body" do
+  it "forwards the standardized body without the raw body" do
     http_client = class_double(Net::HTTP, post: :true)
 
     proxy_service = Compensated::Proxy.new(to: 'http://localhost:8080/compensated-event-listener', http_client: http_client)
 
     proxy_service.handle(request)
-
+    handler = Compensated::PaymentProcessorEventRequestHandler.new(request)
+    data = handler.normalized_event_data
+    data.delete(:raw_body)
     expect(http_client).to have_received(:post).with(
       URI('http://localhost:8080/compensated-event-listener'),
-      '{"raw_event_type":"customer.subscription.updated","raw_event_id":"evt_abc1234","payment_processor":"stripe","amount":{},"customer":{"id":"cus_abcd1234"},"invoice":null,"products":[{"sku":"prod_abcd1234","purchased":"2020-02-07 23:00:00 -0800","quantity":1,"expiration":"2020-05-08 00:00:00 -0700","subscription":{"id":"sub_abcd1234","period":{"start":"2020-04-08 00:00:00 -0700","end":"2020-05-08 00:00:00 -0700"},"status":"active"},"plan":{"sku":"plan_abcd1234","name":"Monthly","interval":{"period":"month","count":1}}}],"timestamp":"2020-04-08 00:00:20 -0700"}',
+      JSON.dump(data),
        "Content-Type" => "application/json"
     )
   end
