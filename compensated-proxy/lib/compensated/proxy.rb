aec19f31fa7caaa8aae5397f68c384cee17246b8
Zee Spencer <50284+zspencer@users.noreply.github.com>
2020-05-17 11:08:02 -0700

    🎨 💡 Document the classes and apply linting
    I'm not sold on rubocop, especially since rubyfmt is so close to  a production release.
    
    However, I am not going to worry too much about it for now.

--- compensated-proxy/lib/compensated/proxy.rb
+++ compensated-proxy/lib/compensated/proxy.rb
@@ -1,30 +1,37 @@
-require "compensated/proxy/version"
+# frozen_string_literal: true
+
+require 'compensated/proxy/version'
 
 require 'net/http'
 
 require 'compensated'
 
-
-
 module Compensated
   class Proxy
+    # Parent class for Proxy errors.
     class Error < StandardError; end
 
-      attr_accessor :to, :http_client
-      def initialize(to: , http_client: Net::HTTP)
-        self.http_client = http_client
-        self.to = URI(to)
-      end
+    attr_accessor :to, :http_client
 
-      def handle(request)
-        handler = Compensated::PaymentProcessorEventRequestHandler.new(request)
-        data = handler.normalized_event_data
-        data.delete(:raw_body)
-        forward(JSON.dump(data))
-      end
+    # @param [String, URI] Downstream event handler that accepts formatted payment events
+    # @param [#post] http_client Object  HTTP transport. Defaults to the `Net::HTTP` constant.
+    def initialize(to:, http_client: Net::HTTP)
+      self.http_client = http_client
+      self.to = URI(to)
+    end
 
-      private def forward(data)
-        http_client.post(to, data, "Content-Type" => "application/json")
-      end
+    # Accepts an HTTP request and forwards it to the downstream event handler
+    # @param [Rack::Request] request Inbound HTTP request with a Payment Gateways'
+    #                                event payload
+    def handle(request)
+      handler = Compensated::PaymentProcessorEventRequestHandler.new(request)
+      data = handler.normalized_event_data
+      data.delete(:raw_body)
+      forward(JSON.dump(data))
+    end
+
+    private def forward(data)
+      http_client.post(to, data, 'Content-Type' => 'application/json')
+    end
   end
 end
