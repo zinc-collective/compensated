c2cc5c354d1137bca2a33962edff56dfc755f70a
Zee Spencer <50284+zspencer@users.noreply.github.com>
2020-06-18 17:09:47 -0700

    Document ContributorSandbox

--- features/contributor-sandbox.js
+++ features/contributor-sandbox.js
@@ -1,11 +1,21 @@
 const { spawn } = require('child-process-promise')
 
-module.exports = class DeveloperSandbox {
+/*
+ * The ContributorSandbox allows us to execute commands on a
+ * per project basis. At present, it's used to execute tests
+ * against multiple language versions, but it could also be
+ * used to bootstrap a contributors developer machine
+ * programmatically and other such nonsense.
+ */
+module.exports = class ContributorSandbox {
   constructor (language, project) {
     this.language = language
     this.project = project
   }
 
+  /*
+   * Spawns a process within the project directory.
+   */
   async spawn (command) {
     const promise = spawn(command, [], {
       cwd: this.project,
@@ -21,6 +31,12 @@ module.exports = class DeveloperSandbox {
     }
   }
 
+  /*
+   * Overrides process.env in spawned processes.
+   *
+   * Useful for injecting different environment flags
+   * when feature testing.
+   */
   get env () {
     return { RBENV_VERSION: this.language.ruby }
   }
